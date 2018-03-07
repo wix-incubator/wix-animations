a01234585f7ca304d454566a7d3312580bd55fc2
Ronen Stern <ronens@wix.com>
2018-03-07 12:41:23 +0200

    Upgrade infra

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,7 +1,7 @@
 const _ = require('lodash/fp');
 const path = require('path');
 const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
-const wixStorybookConfig = require('yoshi/config/webpack.config.storybook');
+const wixStorybookConfig = require('haste-preset-yoshi/config/webpack.config.storybook');
 
 module.exports = (config, env) => {
   const newConfig = wixStorybookConfig(genDefaultConfig(config, env));
