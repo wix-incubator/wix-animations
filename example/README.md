9b25ccda603a68505da789ca3c797e338a84af4a
gendeld <danielge@wix.com>
2020-10-01 18:50:10 +0300

    Remove unrelated files

--- example/README.md
+++
@@ -1,1 +0,0 @@
-# Usage example for wix-animations
--- example/src/client.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Counter from './Counter';
-
-ReactDOM.render(<Counter />, document.getElementById('root'));
